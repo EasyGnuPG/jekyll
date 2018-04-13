63b0b0b98f1b9ae46d28e0b9b2a897d1f520ec2a
jd147 <jaydeep415@gmail.com>
2018-04-14 01:53:47 +0530

    and config change function

--- cmd/project-config.sh
+++ cmd/project-config.sh
@@ -1,18 +1,18 @@
 cmd_project-config_help() {
     cat <<_EOF
-    project-config <project> <post_file_hard_link> 
-         Add config file to given project.
+    project-config <project> 
+         Change config file of given project.
 _EOF
 }
 
 cmd_project-config() {
-    # copy config_file in post dirictory 
+    # edit config_file in post dirictory 
     local project=$1
-    local config=$2
+    
     [[ -n $project ]] || fail "Usage:\n $(cmd_project-config_help)"
-    [[ -n $config ]] || fail "Usage:\n $(cmd_project-config_help)"
     
     
-    yes | cp -rf $config $project/
+    nano $project/_config.yml
+    
     
 }
