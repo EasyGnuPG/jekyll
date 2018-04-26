e752190b218e149ec961067c6c55889d73f194ea
jd147 <jaydeep415@gmail.com>
2018-04-27 00:05:48 +0530

    script-fix

--- settings.sh
+++ settings.sh
@@ -9,8 +9,4 @@ USER=admin
 PASS=pass
 EMAIL="user@example.org"
 
-### Gmail account for notifications.
-### Make sure to enable less-secure-apps:
-### https://support.google.com/accounts/answer/6010255?hl=en
-GMAIL_ADDRESS=
-GMAIL_PASSWD=
+
